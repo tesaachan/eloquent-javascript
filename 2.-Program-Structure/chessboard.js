4a72868feb90d411d37a138d02ef7b5d5078cedd
Ancient Kami <akami@at-m3.msk.21-school.ru>
2021-08-24 17:41:28 +0300

    .

--- 2.-Program-Structure/chessboard.js
+++ 2.-Program-Structure/chessboard.js
@@ -5,16 +5,16 @@ for (let i = 0; i < 8; ++i) {
 	for (let j = 0; j < 8; ++j) {
 		if (i % 2 == 0) {
 			if (j % 2 == 0)
-        		str += ' ';
+        			str += ' ';
 			else
 				str += '#';
-		}
-		else {
-			if (j % 2 == 0)
-				str += '#';
-			else
-        		str += ' ';
-		}
+			}
+			else {
+				if (j % 2 == 0)
+					str += '#';
+				else
+		        	str += ' ';
+			}
 	}
 	console.log (str + '\n');
 }
