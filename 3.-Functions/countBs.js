74fb36aaaacfe27c04d82f90fc9529fbf919e3da
Georgiy Tesarian <86289906+tesaachan@users.noreply.github.com>
2021-08-25 18:20:36 +0300

    Update countBs.js

--- 3.-Functions/countBs.js
+++ 3.-Functions/countBs.js
@@ -6,7 +6,7 @@ function countChar(string, letter) {
 	let count = 0;
 
 	for (let i = 0; i < string.length; ++i) {
-		if (string[i] == letter)
+		if (string[i] === letter)
 			count++;
 	}
 	return (count);
