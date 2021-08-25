ea971e5b59905b8565394b5f01e6432bc1176c13
Ancient Kami <akami@at-h3.msk.21-school.ru>
2021-08-25 17:08:01 +0300

    .

--- 3.-Functions/lecture.js
+++ 3.-Functions/lecture.js
@@ -76,3 +76,78 @@ let twice = multiplier(2);
 console.log(twice(5)); //10
 
 //функция - это значение, содержащее и тело с кодом, и окружение, в котором они созданы
+
+//рекурсия
+
+function power___(base, exponent) {
+	if (exponent == 0) {
+		return 1;
+	} else {
+		return base * power(base, exponent - 1);
+	}
+}
+
+function findSolution(target) {
+	function find(current, history) {
+		if (current == target) {
+			return history;
+		} else if (current > target) {
+			return null;
+		} else {
+			return find(current + 5, `(${history} + 5`) ||
+					find(current * 3, `(${history} * 3`);
+		}
+	}
+	return find(1, "1");
+}
+
+console.log(findSolution(24));
+// (((1 * 3) + 5) * 3)
+
+
+ function printFarmInventory(cows, chickens) {
+	let cowString = String(cows);
+	while (cowString.length < 3) {
+		cowString = "0" + cowString;
+	}
+	console.log(`${cowString} cows`);
+	let chickenString = String(chickens);
+	while (chickenString.length < 3) {
+		chickenString = "0" + chickenString;
+	}
+	console.log(`${chicenString} chickens`);
+}
+printFarmInventory(7, 11); //007 cows 011 chickens
+
+//Вариант правильный с точки зрения функций:
+
+function zeroPad(number, width) {
+	let string = String(number);
+	while (string.length < width) {
+		string = '0' + string;
+	}
+	return string;
+}
+
+function printFarmInventory(cows, chickens, pigs) {
+	console.log(`${zeroPad(cows, 3)} cows`);
+	console.log(`${zeroPad(chickens, 3)} chickens`);
+	console.log(`${zeroPad(pigs, 3)} pigs`);
+}
+
+
+
+//резюме
+
+// 3 вида записи функций:
+// для хранения её значения, объявление и короткая форма
+
+const f = function(a) {
+	console.log(a + 2);
+};
+
+function g(a, b) {
+	return a * b * 3.5;
+}
+
+let h = a => a % 3;
