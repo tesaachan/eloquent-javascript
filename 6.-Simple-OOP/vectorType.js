b40283bc2cd932e963fb714c3e799f8481aa4e0c
tesaachan <sylae@yahoo.com>
2021-08-30 19:09:53 +0300

    update 6

---
+++ 6.-Simple-OOP/vectorType.js
@@ -0,0 +1,22 @@
+class Vec {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	get length() {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	plus(vector) {
+		return new Vec(this.x + vector.x, this.y + vector.y);
+	}
+	minus(vector) {
+		return new Vec(this.x - vector.x, this.y - vector.y);
+	}
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
